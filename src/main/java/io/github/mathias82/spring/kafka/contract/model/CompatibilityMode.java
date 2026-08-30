860b35c430cddafc037b28dde0efe0500952a9c1
Matthaios Stavrou <4541512+mathias82@users.noreply.github.com>
2026-08-30 10:46:35 +0300

    Add full Confluent compatibility modes

--- src/main/java/io/github/mathias82/spring/kafka/contract/model/CompatibilityMode.java
+++ src/main/java/io/github/mathias82/spring/kafka/contract/model/CompatibilityMode.java
@@ -4,7 +4,11 @@ package io.github.mathias82.spring.kafka.contract.model;
  * Defines the compatibility mode enforced against the Schema Registry.
  */
 public enum CompatibilityMode {
+    NONE,
     BACKWARD,
+    BACKWARD_TRANSITIVE,
     FORWARD,
-    FULL
+    FORWARD_TRANSITIVE,
+    FULL,
+    FULL_TRANSITIVE
 }
